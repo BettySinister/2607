3794c2657ce113e1b2799fa4cd8ba5e2e2efe675
BettySinister <abc_def.3000@mail.ru>
2022-10-08 13:51:38 +0300

    Готовое решение

---
+++ 2607.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int array[3][4], counter = 0;
+
+	cout << "{\n";
+	for (int i = 0; i < 3; i++) {
+		cout << "{ ";
+		for (int j = 0; j < 4; j++) {
+			array[i][j] = rand() % 2;
+			if (array[i][j] == 0) {
+				counter++;
+			}
+			cout << array[i][j] << " ";
+		}
+		cout << "}" << endl;
+	}
+	cout << "}" << endl
+		<< "There are " << counter << " zeros.";
+}
